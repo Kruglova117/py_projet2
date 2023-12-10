ff6933dc4cdc30e7792801ae0daac7f80307ccc7
Pashkivska117 <kruglova_katerina>
2023-12-10 17:55:05 +0200

    hm ex-3 done

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-try:
+try:number1 =int(input('Enter ')) 5
+    number2 = 5
+    number3 = 3
+    print(number1 + number2 + number3)
+    print(number1 * number2 * number3)
 
 
 except Exception as e:
