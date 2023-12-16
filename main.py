49af89539fc0bd9785d67c80d0ffbf43af4b2501
Pashkivska117 <kruglova_katerina>
2023-12-16 21:40:57 +0200

    hm ex-2 done

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 try:
+    number_1 = int(input('Введіть зарплату за місяць: '))
+    number_2 = int(input('Введіть платіж за кредит: '))
+    number_3 = int(input(' Введіть платіж за комунальні послуги: '))
+    print( 'Залишок заробітньої плати: ', number_1 - number_2 -number_3)
 
 
 except Exception as e:
