df4c17fc864233a605e54ea159560fd71700c898
Pashkivska117 <kruglova_katerina>
2023-12-16 22:16:42 +0200

    hm ex-3 done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 try:
+    number_1 = float(input('Довжина однієї діагоналі: '))
+    number_2 = int(input('Довжина другої діагоналі: '))
+    print( 'Площа ромба: ', number_1 * number_2 / 2)
 
 
 except Exception as e:
