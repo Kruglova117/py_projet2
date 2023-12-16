a25bc2ee0641d4c4472fa1f26109d97dc526e09b
Pashkivska117 <kruglova_katerina>
2023-12-16 21:05:43 +0200

    hm ex-1 done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-try:number1 =int(input('Enter ')) 5
-    number2 = 5
-    number3 = 3
-    print(number1 + number2 + number3)
-    print(number1 * number2 * number3)
+try:
+    number_1 = float(input("Enter a number 1: "))
+    number_2 = float(input("Enter a number 2: "))
+    number_3 = float(input("Enter a number 3: "))
 
+    print("Sum: ", number_1 + number_2 + number_3)
+    print("Multiplication: ", number_1 * number_2 * number_3)
 
 except Exception as e:
     print(e)
